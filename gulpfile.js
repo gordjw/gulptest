397f9af52649772adad1bab0ab531091eb452f26
Gordon W <gordjw@gmail.com>
2015-06-04 07:59:06 +1000

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+/*!
+ * gulp
+ * $ npm install gulp-ruby-sass gulp-autoprefixer gulp-minify-css gulp-jshint gulp-concat gulp-uglify gulp-imagemin gulp-notify gulp-rename gulp-livereload gulp-cache del --save-dev
+ */
+
+// notify-send workaround
+process.env.DISABLE_NOTIFIER = true; 
+
+// Load plugins
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    //jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+ 
+// Styles
+gulp.task('styles', function() {
+  return sass('src/sass/style.scss', { style: 'expanded' })
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+ 
+// Scripts
+gulp.task('scripts', function() {
+  return gulp.src('src/js/**/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+ 
+// Images
+gulp.task('images', function() {
+  return gulp.src('src/img/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/img'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+ 
+// Clean
+gulp.task('clean', function(cb) {
+    del(['dist/css', 'dist/js', 'dist/assets/img'], cb)
+});
+ 
+// Default task
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images');
+});
+ 
+// Watch
+gulp.task('watch', function() {
+ 
+  // Watch .scss files
+  gulp.watch('src/sass/**/*.scss', ['styles']);
+ 
+  // Watch .js files
+  gulp.watch('src/js/**/*.js', ['scripts']);
+ 
+  // Watch image files
+  gulp.watch('src/img/**/*', ['images']);
+ 
+  // Create LiveReload server
+  livereload.listen();
+ 
+  // Watch any files in dist/, reload on change
+  gulp.watch(['dist/**']).on('change', livereload.changed);
+ 
+});
+
---
+++ src/js/site.js
@@ -0,0 +1,6 @@
+jQuery(document).ready( function($) {
+	if(console) {
+		console.log("Hello, world");
+
+	}
+});
